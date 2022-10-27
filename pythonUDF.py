e99d4e53c798f12cd3d965b595ab8934ce17f2d3
Veradiva <88526760+Veradiva@users.noreply.github.com>
2022-10-27 05:19:10 +0100

    python codes

---
+++ pythonUDF.py
@@ -0,0 +1,30 @@
+'''def sum(a,b,c):
+    print(a+b+c)
+summer =sum(5,10,15)
+print(summer)
+print (summer*5)'''
+
+'''def sum (a,b,c):
+    a+b+c
+    
+summer= sum(5,10,15)
+print(summer)'''
+
+'''def sum (a,b,c):
+    print(a+b+c)
+    return a+b+c
+summer =sum(5,10,15)
+#print(summer)#
+print(summer*5)'''
+
+#sqrList =map(lambda x: x*x,[1,2,3,4])
+
+
+def highest(a,b):
+    if a > b:
+        return  'True' 
+    else:
+        return 'False'
+x=highest(80002,9)
+print(x)
+    
